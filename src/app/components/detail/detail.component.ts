0f2b77bc194c454bf5ea6a8b09ab6962e12b305d
Kaan Uygur <previousdeveloper@gmail.com>
2018-06-11 14:25:51 +0300

    added detail routing

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import * as algoliasearch from 'algoliasearch';
 
 @Component({
   selector: 'app-detail',
@@ -7,13 +8,27 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit {
+  car;
+  index;
+  constructor(private route: ActivatedRoute) {
+    var client = algoliasearch('XLOY9IMVIL', '86abf6ed1a3a1b06e3770346fe03f8dd');
+    this.index = client.initIndex('28feb');
 
-  constructor(private route: ActivatedRoute) { }
+    //  index.getObject("1496380", function(err, content){
+    //    if(err) throw err;
+    //    console.log(content);
+    //  })
+   }
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
-      console.log(params);
+    const param = this.route.snapshot.paramMap.get('id');
+    this.index.getObject(param, function(err, content){
+      if(err) throw err;
+      console.log(content);
     })
-  }
-
+    console.log(param);
+    // this.route.params.subscribe(params => {
+    //   console.log(params);
+    this.car = param;
+    }
 }
